798deb48eab7e08abce6dc8a831f08871d4d71ce
cedricng <ngoupandecedric@hotmail.fr>
2022-11-01 21:16:46 +0100

    Delete src/environments directory

--- src/environments/environment.prod.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-  production: true,
-  weatherApiBaseUrl: 'https://open-weather13.p.rapidapi.com/city',
-  XRapidAPIHostHeaderName :'X-RapidAPI-Host',
-  XRapidAPIHostHeaderValue : 'open-weather13.p.rapidapi.com',
-  XRapidAPIKeyHeaderName : 'X-RapidAPI-Key' ,
-  XRapidAPIKeyHeaderValue : 'a59d3069e2msh807260c577870f9p175166jsn6dfe72ba1a37'
-};
